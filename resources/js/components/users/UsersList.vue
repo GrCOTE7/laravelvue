64d0e0303f67233e82c6488845d0ed9f1383ca8b
Lionel COTE <grcote7@gmail.com>
2023-06-01 16:45:27 +0200

    Feat: User list with their books (vue)

--- resources/js/components/users/UsersList.vue
+++ resources/js/components/users/UsersList.vue
@@ -1,7 +1,13 @@
 <template>
     <template v-for="user in users">
-        <p><span v-text="user.id" /> -
-        <span v-text="user.name" class="font-bold" /></p>
+        <p>
+            <span v-text="user.id" /> -
+            <span
+                v-text="user.name"
+                style="font-weight: 600"
+                :title="getBooks(user)"
+            />
+        </p>
     </template>
 </template>
 
@@ -9,12 +15,29 @@
 import { ref } from "vue";
 import axios from "axios";
 
+const getBooks= (user)=>{
+    let books = user.books;
+
+    // console.log(typeof books)
+
+    if (books.length) {
+        return books.map(book=>{
+            return (book.title);
+        })
+    }
+        else {
+            return "No book\n\nyet"
+        };
+
+}
+
 const users = ref([]);
 
 const loadFromServer = async (): Promise<void> => {
     await axios
         .get("api/users")
         .then((res) => (users.value = res.data.data))
+        .then((res) => console.log(res))
         .catch((err) => console.log(err));
 };
 loadFromServer();
